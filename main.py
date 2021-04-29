3aee2f6f0fad32275810047c816969a1fb003505
tweety <arinkka7@mail.ru>
2021-04-29 18:08:32 +0000

    numpy+

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import math
 import time
+import numpy as np
 sp_o = [] #список открытых = непроверенных вершин - тут все дети
 sp_z = [] # список проверенных = уже встречаемых = закрытых вершин
+sp_o = np.array([])
 
 
 class Node:
@@ -15,15 +18,15 @@ class Node:
         self.must_be_str = str(must_be(self.size)).split('/')
 
         # self.h = self.ves_h()
-        self.h = self.ves_pifag()
-        # self.h = self.ves_Manhattan()
+        # self.h = round(self.ves_pifag())
+        self.h = round(self.ves_Manhattan())
         self.f = self.h + self.g
         # print(self.f)
     def ves_h(self): #кол-во цифр не на своем месте
         ves = 0
         for i in range(len(self.node)):
             if self.node[i] != self.must_be_str[i]:
-                ves +=1
+                ves += 1
         # print('from Node===ok')
         return ves
     def ves_Manhattan(self):
@@ -55,7 +58,7 @@ class Node:
             # print(i)
             for j in range(df_1.shape[1]):
                 # print(j)
-                num = df_1.iloc[i,j]
+                num = df_1.iloc[i, j]
                 # print(num)
                 ni = df_must_be[df_must_be == num].index[0]
                 nj = df_must_be[df_must_be == num].index[1]
@@ -103,7 +106,7 @@ class Node:
                 num = df_1.iloc[i, j]
                 ni = df_must_be[df_must_be == num].index[0]
                 nj = df_must_be[df_must_be == num].index[1]
-                ves += ((i - ni)**2 + (j - nj)**2)**0.5
+                ves += math.sqrt((i - ni) * (i - ni) + (j - nj) * (j - nj))
         return ves
 
 
@@ -137,6 +140,7 @@ def make_children(list_p, n):
 #     per_l - лист исходного состояния 15нашек
 #     sun - выходной список сыновей
     i_0 = per_l.index('0')
+
     # print('len per_l', len(per_l), i_0, [i_0 - n, i_0 - 1, i_0 + 1, i_0 + n], 'n=',n)
     if n >= 3:
         sun = []
@@ -196,19 +200,40 @@ def must_be(n):
     # print(st.split('/'))
     return st
 
-def check_min_ves(spis_node_open, spis_node_z):
-    sp_node_z = [sp_z[i].node for i in range(len(sp_z))]
+def check_min_ves(sp_o, sp_node_z):
+    # sp_node_z = [sp_z[i].node for i in range(len(sp_z))]
+    # min_v_node = 0
+    # for node in spis_node_open:
+    #     if node.node not in sp_node_z:
+    #         min_v_node = node
+    # if spis_node_open[0].node not in sp_node_z:
+    # min_v_node = spis_node_open[0]
+    sp_f_o = [int(sp_o[i].f) for i in range(len(sp_o))]
+    print(sp_f_o)
+    min_smfo = sp_f_o[0]
+    for i in sp_f_o:
+        if i < min_smfo:
+            min_smfo = i
+    i = sp_f_o.index(min_smfo)
+    return sp_o[i]
+
+
     min_v_node = 0
-    for node in spis_node_open:
-        if node.node not in sp_node_z:
-            min_v_node = node
-
-
-    if spis_node_open[0].node not in sp_node_z:
-        min_v_node = spis_node_open[0]
-    for node in spis_node_open:
-        if node.node not in sp_node_z and node.f < min_v_node.f:
-            min_v_node = node
+    # for node in sp_o:
+    #     if type(min_v_node) == type(node):
+    #         if node.node not in sp_node_z and node.f < min_v_node.f:
+    #             min_v_node = node
+    #             return min_v_node
+    #     else: #int и node
+    #         if node.node not in sp_node_z:
+    #             min_v_node = node
+    #         else:
+    #             min_v_node = 0
+            # if type(min_v_node) == type(node):
+            #     if node.node not in sp_node_z and node.f <= min_v_node.f:
+            #         min_v_node = node
+            # else:
+            #     min_v_node =
     return min_v_node
 
 def path_print(min, sp_z):
@@ -269,12 +294,13 @@ if (n_str - 1) != size_matr:
 print("read file = ok")
 print("bb ' ",bb)
 b = bb
-
+childrens = 0
 
 
 # b = "/".join(b.split())
 # print(b)
 ch = make_children(b.split("/"), size_matr) #  для исходного состояния рождаем детей(макс 4)
+childrens += 1
 # и добавляем всех в список открытых вершин
 import sys
 
@@ -282,7 +308,7 @@ import sys
 A = Node(size_matr, None, b.split("/"), 0)
 # A2 = Node(size_matr, None, b.split("/"), n_st)
 # print(A is A2)
-sp_o.append(A)
+sp_o = np.append(sp_o, A)
 if A.must_be_str == A.node:
     print("исходное состояние == конечному")
     sys.exit()
@@ -295,12 +321,20 @@ for c in ch:
     sp_o_node = [sp_o[i].node for i in range(len(sp_o))]
     sp_z_node = [sp_z[i].node for i in range(len(sp_z))]
     if ch_c.node not in sp_o_node and ch_c.node not in sp_z_node:
-        sp_o.append(ch_c)
+        sp_o = np.append(sp_o, ch_c)
 #  после заполнения ночальных условий
-sp_z.append(A) # создали всех детей == закрыли
-while sp_o:
+sp_z.append(A) # создали всех детей == закрыли вершину
+# sp_o.remove(A)
+# i = np.where(a == A)
+i = list(sp_o).index(A)
+sp_o = np.delete(sp_o, i)
+print(sp_o)
+
+while len(sp_o) >= 1:
 #     1. найти в  открытом списке вершину с минимальным весом
-        min = check_min_ves(sp_o, sp_z)
+        min = check_min_ves(sp_o, sp_z_node)
+        # min = sp_o[0]
+        print(min.f, min.g, min.h)
         if min == 0:
             print("нет открытых вершин")
             sys.exit()
@@ -308,9 +342,9 @@ while sp_o:
 
             # if min.node not  in [sp_z[i].node for i in range(len(sp_z))]: # добавлять еще надо только в том случае если
                 #нет или путь до короче    or ???
-            sp_z.append(min)
 
             child = make_children(min.node,size_matr)
+            childrens += 1
             for c in child: # вероятно тоже надо проверить что б ещене было
                 # или путь новой короче
                 # хотя путь короче ищем при переборе списка
@@ -319,12 +353,18 @@ while sp_o:
                 sp_z_node = [sp_z[i].node for i in range(len(sp_z))]
                 sp_z_ves = [sp_z[i].f for i in range(len(sp_z))]
                 ch_c = Node(size_matr,min,c.split("/"), min.g + 1)
+                # print(ch_c.f)
                 if ch_c.node not in sp_o_node and ch_c.node not in sp_z_node:
-                    sp_o.append(ch_c) # нет ни в закрытом, ни в открытом
+                    sp_o = np.append(sp_o, ch_c) # нет ни в закрытом, ни в открытом
                 elif ch_c.node in sp_z_node:
                     ii = sp_z_node.index(ch_c.node)
                     if sp_z_ves[ii] > ch_c.f:
                         sp_z[ii] = ch_c  # заменим,если вес был больше
+            sp_z.append(min)
+            # sp_o.remove(min)
+            i = list(sp_o).index(min)
+            sp_o = np.delete(sp_o, i)
+
                 # if ch_c.node in sp_o_node:
                 #     ii = sp_o_node.index(ch_c.node)
                 #     if sp_o_ves[ii] > ch_c.f:
@@ -342,7 +382,7 @@ while sp_o:
                 # if ch_c.node not in sp_o_node and c not in sp_z_node:
                     # если нет в закрытых
                     # проверить
-            # sp_o.remove(min)
+
             # sp_o.extend(child)
 #     2. если не конечное состояние
 #         - добавим к закрытым вершинам
@@ -353,13 +393,16 @@ while sp_o:
             print("КОНЕЦ", min.node)
             path_print(min, sp_z)
 
-            sp_o.remove(min)
+            # sp_o.remove(min)
+            i = list(sp_o).index(min)
+            sp_o = np.delete(sp_o, i)
             sp_o = []
             # print(sp_z)
             print("всё оке")
 t_2 = time.time()
 dt = t_2 - t_1
 print("время = ", dt)
+print(childrens)
 #     если конечное - выходим - востанавливаем ролдителей всего пути?
 #  при том каждую аершину берем с минимальным g весом???
 #
